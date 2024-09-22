dde2c70d53b89f74edd9c8b25eb11b5fd2f9df8c
Reaosen <qls18230040072@163.com>
2024-09-22 14:50:26 +0800

    add login without refresh

--- src/main/java/codingforlove/community/Controller/QuestionController.java
+++ src/main/java/codingforlove/community/Controller/QuestionController.java
@@ -1,5 +1,6 @@
 package codingforlove.community.Controller;
 
+import codingforlove.community.DTO.ResultDTO;
 import codingforlove.community.Service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -14,22 +14,31 @@ function post() {
             if (response.code === 200) {
                 $("#comment_section").hide();
             } else {
-                alert(response.message);
+                if (response.code === 2003) {
+                    let isAccepted = confirm(response.message);
+                    if (isAccepted) {
+                        window.open("https://gitee.com/oauth/authorize?client_id=7e0df6b4bb3e4edc48e84259b8cd61a4780a8669cb002c149921c33a79f45530&redirect_uri=http://localhost:8080/giteeCallback&response_type=code");
+                        window.localStorage.setItem("closable", true);
+                    }
+                } else {
+                    alert(response.message);
+                }
             }
+
+
         },
         dataType: "json"
     });
 }
 
+// todo 页面不进行跳转
 function deleteQuestion() {
     let questionId = $("#delete_id").val();
     $.ajax({
         type: "DELETE",
         url: "/question/" + questionId + "/delete",
         success: function (response) {
-            if (response.code === 200) {
-                window.location.href = "/";
-            }else {
+            if (response.code !== 200) {
                 alert(response.message);
             }
         }
